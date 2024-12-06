c7e137e5c140ef9855d813af4ae7eff08f0cb3e7
Ashish <ashishdhola0.7@gmail.com>
2024-12-06 16:38:46 +0530

    Crud with mongodb

---
+++ crud with mongodb(database)/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const dbConnect = require('./config/dbConnection');
+const port = 1595;
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded());
+
+app.use('/user', require('./routes/userRoutes'));
+
+app.get('/', (req, res) => {
+    return res.render('index');
+})
+
+app.listen(port, () => {
+
+
+    console.log(`Server Start at http://localhost:${port}`);
+})
